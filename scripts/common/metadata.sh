60b4ec092bf7b647e064d0b1b6ae2e6c1673b6de
Ryan Breed <opensource@breed.org>
2016-12-29 10:22:26 -0600

    initial box

--- scripts/common/metadata.sh
+++ scripts/common/metadata.sh
@@ -1,6 +1,13 @@
 #!/bin/sh -eux
 
 mkdir -p /etc;
-cp /tmp/bento-metadata.json /etc/bento-metadata.json;
+cat <<END > /etc/bento-metadata.json
+{
+  "bento_name":"$BENTO_NAME",
+  "bento_base":"$BENTO_BOX",
+  "bento_template":"$BENTO_TEMPLATE",
+  "bento_version":"$BENTO_VERSION",
+  "bento_timestamp":"$BENTO_TIMESTAMP"
+}
+END
 chmod 0444 /etc/bento-metadata.json;
-rm -f /tmp/bento-metadata.json;
