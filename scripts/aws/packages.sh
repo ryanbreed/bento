dad3f8ef8bed870d264af4ac7dd069eb81ef1941
Ryan Breed <opensource@breed.org>
2017-01-30 22:08:02 -0600

    rpm bad; zip good

--- scripts/aws/packages.sh
+++ scripts/aws/packages.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
-yum -y install NetworkManager audit audispd-plugins bzip2 deltarpm gcc cpp perl authconfig kernel-devel kernel-headers make  nfs-utils ntp openssh-clients unzip vim-minimal xz ruby grub2-tools
-yum -y install http://s3.amazonaws.com/ec2-downloads/ec2-ami-tools.noarch.rpm
-
-echo "rewriting amitools binstubs"
-
-cat <<END > /usr/bin/ec2-bundle-vol
-#!/bin/bash
-export RUBYLIB=\$RUBYLIB:/usr/lib/ruby/site_ruby:/usr/lib64/ruby/site_ruby
-ruby /usr/lib/ruby/site_ruby/ec2/amitools/bundlevol.rb \$*
+yum -y install NetworkManager audit audispd-plugins bzip2 deltarpm gcc cpp perl authconfig kernel-devel kernel-headers make  nfs-utils ntp openssh-clients unzip vim-minimal xz ruby grub2-tools gdisk kpartx parted python2-pip python-virtualenv
+mkdir -p /opt/ec2
+cat <<END > /etc/profile.d/ec2-tools.sh
+export PATH=\$PATH:/opt/ec2/bin
+export EC2_AMITOOL_HOME=/opt/ec2
 END
+chmod 0755 /etc/profile.d/ec2-tools.sh
 
-chmod 755 /usr/bin/ec2-bundle-vol
-rm -f /usr/local/bin/ec2-bundle-vol
-
-cat <<END > /usr/bin/ec2-upload-bundle
-#!/bin/bash
-export RUBYLIB=\$RUBYLIB:/usr/lib/ruby/site_ruby:/usr/lib64/ruby/site_ruby
-ruby /usr/lib/ruby/site_ruby/ec2/amitools/uploadbundle.rb \$*
-END
-
-chmod 755 /usr/bin/ec2-upload-bundle
-rm -f /usr/local/bin/ec2-upload-bundle
-
-
+( cd /var/tmp && curl -L -O http://s3.amazonaws.com/ec2-downloads/ec2-ami-tools.zip && unzip ec2-ami-tools.zip -d /opt/ec2 )
 
