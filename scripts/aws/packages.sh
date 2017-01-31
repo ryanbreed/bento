3bbde225dd15b8be8bf927b05e2cdc4cd553100b
Ryan Breed <opensource@breed.org>
2017-01-30 19:38:11 -0600

    update aws base packages

--- scripts/aws/packages.sh
+++ scripts/aws/packages.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-yum -y install NetworkManager \ 
-       audit audispd-plugins \
-       bzip2 deltarpm gcc cpp perl authconfig kernel-devel kernel-headers make \
-       nfs-utils ntp openssh-clients unzip vim-minimal \
-       xz yum-cron
+yum -y install NetworkManager audit audispd-plugins bzip2 deltarpm gcc cpp perl authconfig kernel-devel kernel-headers make  nfs-utils ntp openssh-clients unzip vim-minimal xz
