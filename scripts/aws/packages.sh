b24afd00176711e42c531132ac8e4f882a5cf11d
Ryan Breed <opensource@breed.org>
2017-02-01 21:44:45 -0600

    php; aws packages

--- scripts/aws/packages.sh
+++ scripts/aws/packages.sh
@@ -9,3 +9,5 @@ chmod 0755 /etc/profile.d/ec2-tools.sh
 
 ( cd /var/tmp && curl -L -O http://s3.amazonaws.com/ec2-downloads/ec2-ami-tools.zip && unzip ec2-ami-tools.zip -d /opt/ec2 )
 
+yum-config-manager --save --setopt=extras.enabled=1
+yum install cloud-utils euca2ools 
